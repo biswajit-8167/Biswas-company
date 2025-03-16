b1d26da5cc59104ec7eef141f0357e6bc6110f7e
Biswajit Roy <bbiswajitroy8167@gmail.com>
2025-03-17 01:08:52 +0530

    get all

--- controller/getAllPayment.js
+++ controller/getAllPayment.js
@@ -1,37 +1,4 @@
-// const UserModel = require("../modules/userSchema");
-
-// async function getAllPaymentsController(req, res) {
-//     try {
-//         // Get the user ID from the authenticated request
-//         const userId = req.user._id;
-
-//         // Find the user and fetch their payments
-//         const user = await UserModel.findById(userId).select('payments');
-
-//         if (!user) {
-//             throw new Error("User not found");
-//         }
-
-//         // Respond with the list of payments
-//         res.status(200).json({
-//             data: user.payments,
-//             message: "Payments fetched successfully",
-//             error: false,
-//             success: true,
-//         });
-//     } catch (error) {
-//         res.status(400).json({
-//             message: error.message || error,
-//             error: true,
-//             success: false,
-//         });
-//     }
-// }
-
-// module.exports = getAllPaymentsController;
-
-
-
+ 
 const UserModel = require("../modules/userSchema");
 
 async function getAllUserPaymentsController(req, res) {
