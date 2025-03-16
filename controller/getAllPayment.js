e27d06028d6d400fb8a136e7d2811d719fed1a20
Biswajit Roy <bbiswajitroy8167@gmail.com>
2025-03-17 01:02:54 +0530

    11th

--- controller/getAllPayment.js
+++ controller/getAllPayment.js
@@ -1,21 +1,48 @@
+// const UserModel = require("../modules/userSchema");
+
+// async function getAllPaymentsController(req, res) {
+//     try {
+//         // Get the user ID from the authenticated request
+//         const userId = req.user._id;
+
+//         // Find the user and fetch their payments
+//         const user = await UserModel.findById(userId).select('payments');
+
+//         if (!user) {
+//             throw new Error("User not found");
+//         }
+
+//         // Respond with the list of payments
+//         res.status(200).json({
+//             data: user.payments,
+//             message: "Payments fetched successfully",
+//             error: false,
+//             success: true,
+//         });
+//     } catch (error) {
+//         res.status(400).json({
+//             message: error.message || error,
+//             error: true,
+//             success: false,
+//         });
+//     }
+// }
+
+// module.exports = getAllPaymentsController;
+
+
+
 const UserModel = require("../modules/userSchema");
 
-async function getAllPaymentsController(req, res) {
+async function getAllUserPaymentsController(req, res) {
     try {
-        // Get the user ID from the authenticated request
-        const userId = req.user._id;
+        // Fetch all users and their payments
+        const users = await UserModel.find().select('username email payments');
 
-        // Find the user and fetch their payments
-        const user = await UserModel.findById(userId).select('payments');
-
-        if (!user) {
-            throw new Error("User not found");
-        }
-
-        // Respond with the list of payments
+        // Respond with the list of users and their payments
         res.status(200).json({
-            data: user.payments,
-            message: "Payments fetched successfully",
+            data: users,
+            message: "All user payments fetched successfully",
             error: false,
             success: true,
         });
@@ -28,4 +55,4 @@ async function getAllPaymentsController(req, res) {
     }
 }
 
-module.exports = getAllPaymentsController;
+module.exports = getAllUserPaymentsController;
